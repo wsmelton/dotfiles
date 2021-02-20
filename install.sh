0d55edb923e6a4e49851bf143066e2f05a0b1cfb
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-02-20 13:15:32 -0600

    try to make it update pwsh

--- install.sh
+++ install.sh
@@ -6,4 +6,10 @@ then
     wget -O - https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash -s
 fi
 
+# if installed lets update it
+if command -v pwsh &> /dev/null
+then
+    sudo apt-get install powershell
+fi
+
 pwsh -nologo -noprofile -command ./install.ps1
